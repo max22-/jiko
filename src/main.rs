df46e6d3769b58abfe16e0308d8a1bcc64caec6e
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2022-02-17 22:01:18 +0100

    Improved the parser

--- src/main.rs
+++ src/main.rs
@@ -41,18 +41,52 @@ impl fmt::Display for JkProgram {
     }
 }
 
+struct JkList(Vec<JkProgram>);
+type JkStack = JkList;
+type JkQueue = JkList;
+
+impl fmt::Display for JkList {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "[")?;
+            write!(
+                f,
+                "{}",
+                self.0
+                    .iter()
+                    .map(|p| p.to_string())
+                    .collect::<Vec<String>>()
+                    .join(", ")
+            )?;
+            write!(f, "]")
+    }
+}
+
 use pest::Parser;
 
 #[derive(Parser)]
 #[grammar = "jiko.pest"]
 pub struct JkParser;
 
+fn parse(input: &str) -> JkQueue
+{
+    let pest_output = JkParser::parse(Rule::input, input).expect("Parse error").next().unwrap();
+
+
+    let mut res: JkQueue = JkList(vec!());
+
+    for program in pest_output.into_inner() {
+        for p in program.into_inner() {
+            match p.as_rule() {
+                Rule::integer => res.0.push(JkInt(p.as_str().parse::<i64>().unwrap())),
+                Rule::word => res.0.push(JkWord(p.as_str().to_string())),
+                Rule::EOI => (),
+                _ => unreachable!(),
+            }
+        }
+    }
+    res
+}
+
 fn main() {
-    let program = JkQuotation(vec![JkInt(1), JkInt(2), JkWord("+".to_string())]);
-
-    println!("Program:");
-    println!("{}", program);
-
-    let parse = JkParser::parse(Rule::input, "1234");
-    println!("parse: {:?}", parse);
+    println!("{}", parse("1 2 add 3 sub"));
 }
