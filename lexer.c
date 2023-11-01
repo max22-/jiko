3c980f58d6f466a8d9e32289eb571da7c7c99a3f
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2023-11-01 17:40:09 +0100

    Formatting

--- lexer.c
+++ lexer.c
@@ -1,10 +1,10 @@
 #include "lexer.h"
-#include <assert.h>
 #include "misc.h"
+#include <assert.h>
+#include <ctype.h> /* for isspace */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h> /* for isspace */
 
 /* Token *********************************************************************/
 
@@ -20,7 +20,7 @@ token_t *token_new(token_type type, const char *value, unsigned int line,
 }
 
 void token_free(token_t *token) {
-    free((void*)token->value);
+    free((void *)token->value);
     free(token);
 }
 
@@ -46,7 +46,7 @@ void token_print(token_t *token, void (*printer)(const char *)) {
 /* Lexer *********************************************************************/
 
 static void next_char(lexer_t *lex) {
-    if(lex->text[lex->pos] == '\n') {
+    if (lex->text[lex->pos] == '\n') {
         lex->pos_line++;
         lex->pos_column = 1;
     } else {
@@ -62,14 +62,14 @@ static void update_start(lexer_t *lex) {
 }
 
 static void skip_space(lexer_t *lex) {
-    while(lex->pos < lex->text_len && isspace(lex->text[lex->pos]))
+    while (lex->pos < lex->text_len && isspace(lex->text[lex->pos]))
         next_char(lex);
     update_start(lex);
 }
 
 /* Duplicates text */
 lexer_t *lexer_new(const char *text) {
-    lexer_t *res = (lexer_t*)malloc(sizeof(lexer_t));
+    lexer_t *res = (lexer_t *)malloc(sizeof(lexer_t));
     assert(res && "lexer_new: malloc failed");
     res->text = strdup(text);
     res->text_len = strlen(text);
@@ -79,26 +79,28 @@ lexer_t *lexer_new(const char *text) {
     return res;
 }
 
-
 static token_t *open_bracket(lexer_t *lex) {
-    token_t *res = token_new(TOK_OPEN_BRACKET, "[", lex->start_line, lex->start_column);
+    token_t *res =
+        token_new(TOK_OPEN_BRACKET, "[", lex->start_line, lex->start_column);
     next_char(lex);
     update_start(lex);
     return res;
 }
 
 static token_t *close_bracket(lexer_t *lex) {
-    token_t *res = token_new(TOK_CLOSE_BRACKET, "]", lex->start_line, lex->start_column);
+    token_t *res =
+        token_new(TOK_CLOSE_BRACKET, "]", lex->start_line, lex->start_column);
     next_char(lex);
     update_start(lex);
     return res;
 }
 
 static token_t *integer(lexer_t *lex) {
-    while(lex->pos < lex->text_len && isdigit(lex->text[lex->pos]))
+    while (lex->pos < lex->text_len && isdigit(lex->text[lex->pos]))
         next_char(lex);
     char *strval = strndup(&lex->text[lex->start], lex->pos - lex->start);
-    token_t *res = token_new(TOK_INTEGER, strval, lex->start_line, lex->start_column);
+    token_t *res =
+        token_new(TOK_INTEGER, strval, lex->start_line, lex->start_column);
     update_start(lex);
     free(strval);
     return res;
@@ -106,16 +108,18 @@ static token_t *integer(lexer_t *lex) {
 
 static token_t *string(lexer_t *lex) {
     next_char(lex); /* we match the '"' */
-    while(1) {
-        if(lex->pos >= lex->text_len)
-            return token_new(TOK_ERROR, "Unexpected EOF in string literal", lex->start_line, lex->start_column);
-        if(lex->text[lex->pos] == '"')
+    while (1) {
+        if (lex->pos >= lex->text_len)
+            return token_new(TOK_ERROR, "Unexpected EOF in string literal",
+                             lex->start_line, lex->start_column);
+        if (lex->text[lex->pos] == '"')
             break;
-        if(lex->text[lex->pos] == '\\') {
+        if (lex->text[lex->pos] == '\\') {
             next_char(lex);
-            if(lex->pos >= lex->text_len)
-                return token_new(TOK_ERROR, "Unexpected EOF in string literal", lex->start_line, lex->start_column);
-            switch(lex->text[lex->pos]) {
+            if (lex->pos >= lex->text_len)
+                return token_new(TOK_ERROR, "Unexpected EOF in string literal",
+                                 lex->start_line, lex->start_column);
+            switch (lex->text[lex->pos]) {
             case 'n':
             case 'r':
             case 't':
@@ -123,7 +127,8 @@ static token_t *string(lexer_t *lex) {
             case '\\':
                 break;
             default:
-                return token_new(TOK_ERROR, "Invalid escaped character", lex->start_line, lex->start_column);
+                return token_new(TOK_ERROR, "Invalid escaped character",
+                                 lex->start_line, lex->start_column);
             }
         }
         next_char(lex);
@@ -131,39 +136,40 @@ static token_t *string(lexer_t *lex) {
     assert(lex->text[lex->pos] == '"'); /* should always be the case */
     next_char(lex);
     char *strval = strndup(&lex->text[lex->start], lex->pos - lex->start);
-    token_t *res = token_new(TOK_STRING, strval, lex->start_line, lex->start_column);
+    token_t *res =
+        token_new(TOK_STRING, strval, lex->start_line, lex->start_column);
     update_start(lex);
     free(strval);
     return res;
 }
 
 static token_t *word(lexer_t *lex) {
-    while(1) {
-        if(lex->pos >= lex->text_len)
+    while (1) {
+        if (lex->pos >= lex->text_len)
             break;
         char c = lex->text[lex->pos];
-        if(isspace(c) || c == '[' || c == ']')
+        if (isspace(c) || c == '[' || c == ']')
             break;
         next_char(lex);
     }
     char *strval = strndup(&lex->text[lex->start], lex->pos - lex->start);
-    token_t *res = token_new(TOK_WORD, strval, lex->start_line, lex->start_column);
+    token_t *res =
+        token_new(TOK_WORD, strval, lex->start_line, lex->start_column);
     lex->start = lex->pos;
     free(strval);
     return res;
-
 }
 
 token_t *lexer_next(lexer_t *lex) {
     skip_space(lex);
-    if(lex->pos >= lex->text_len)
+    if (lex->pos >= lex->text_len)
         return token_new(TOK_EOF, "", lex->start_line, lex->start_column);
     char c = lex->text[lex->pos];
-    if(c == '[')
+    if (c == '[')
         return open_bracket(lex);
-    else if(c == ']')
+    else if (c == ']')
         return close_bracket(lex);
-    else if(isdigit(c))
+    else if (isdigit(c))
         return integer(lex);
     else if (c == '"')
         return string(lex);
--- misc.c
+++ misc.c
@@ -11,7 +11,7 @@ char *strdup(const char *str) {
 
 char *strndup(const char *str, size_t n) {
     size_t len;
-    for(len = 0; len < n && str[len]; len++);
+    for (len = 0; len < n && str[len]; len++);
     char *res = malloc(len);
     assert(res && "strndup: malloc failed");
     memcpy(res, str, len);
