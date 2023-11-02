6aa72152665ffed91d0ea10f2b0246cd686de17e
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2023-11-02 23:12:53 +0100

    Corrected error in strndup (spotted by valgrind)

--- misc.c
+++ misc.c
@@ -12,7 +12,7 @@ char *strdup(const char *str) {
 char *strndup(const char *str, size_t n) {
     size_t len;
     for (len = 0; len < n && str[len]; len++);
-    char *res = malloc(len);
+    char *res = malloc(len + 1);
     assert(res && "strndup: malloc failed");
     memcpy(res, str, len);
     res[len] = 0;
